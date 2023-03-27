9f3d6530d3338a8d17e41814996fdf6b393d9608
rkes <rakesh.nit.mca@gmail.com>
2023-03-27 15:11:09 +0530

    Create webpush-service-worker.js

---
+++ webpush-service-worker.js
@@ -0,0 +1,3 @@
+importScripts('https://api.pushio.com/webpush/sdk/service_min.js');
+var config = '{"appserviceKey":"BIgYnJ5tqIj-I4N2-09SNhfG7plm23UKkcBMgzCNSzaX3aTkPnE_twKkJAtNy40TVykDHAZL6zbSlesCLxAbwVw=","apiKey":"ABEO8pKlSqVYUSP6k0r0r0po4","accountToken":"ABEmaWugGfUbcIOzzvL7O18Bk","appver":"0.0.0","apiHost":"https://api.pushio.com","lazy":false}';
+orawp.init(config);
